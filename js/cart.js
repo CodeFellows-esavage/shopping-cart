5c9bacc88be0af930826a218b9a7df950fe8f60f
erik savage <erik.k.savage@gmail.com>
2021-11-11 18:37:52 -0800

    cart.js updates

--- js/cart.js
+++ js/cart.js
@@ -8,9 +8,9 @@ let cart;
 
 function loadCart() {
   const cartItems = JSON.parse(localStorage.getItem('cartKey')) || [];
-  // cart = new Cart(cartItems); 
-  cart = cartItems;
-  //console.log(cartItems);
+  cart = new Cart(cartItems); 
+  // cart = cartItems;
+  // console.log(cartItems);
 }
 
 // Make magic happen --- re-pull the Cart, clear out the screen and re-draw it
@@ -22,20 +22,22 @@ function renderCart() {
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
 function clearCart() {
-
-
+  const tbodyElem = document.querySelector('tbody');
+  while(tbodyElem.firstChild) {
+    tbodyElem.removeChild(tbodyElem.firstChild);
+  }
 }
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
 
   // DONE?: Find the table body
-      const tbodyElem = document.querySelector('tbody');
+  const tbodyElem = document.querySelector('tbody');
       
 
 
   // DONE: Iterate over the items in the cart
-  for (let i = 0; i < 3; i += 1){
+  for (let i = 0; i < cart.items.length; i += 1){
       // DONE: Create a TR
   const trElem = document.createElement('tr');
   tbodyElem.appendChild(trElem);
@@ -44,15 +46,16 @@ function showCart() {
   const tdELem0 = document.createElement('td');
   trElem.appendChild(tdELem0);
   tdELem0.setAttribute('id', `x-button${i}`);
-  tdELem0.textContent = 'X-Button';
+  tdELem0.setAttribute('class', 'xbtn');
+  tdELem0.textContent = 'X';
 
   const tdELem1 = document.createElement('td');
   trElem.appendChild(tdELem1);
-  tdELem1.textContent = `Quantity: ${cart.items[i].quantity}`;
+  tdELem1.textContent = cart.items[i].quantity;
 
   const tdELem2 = document.createElement('td');
   trElem.appendChild(tdELem2);
-  tdELem2.textContent = `Product: ${cart.items[i].product}`;
+  tdELem2.textContent = cart.items[i].product;
   
    
   }
@@ -64,13 +67,19 @@ function showCart() {
 }
 
 function removeItemFromCart(event) {  //this is called when delete link is clicked
-    cart.removeItemFromCart(Cart)
+  const id = event.target.id;
+  const xbtn = id.slice(0, 8);
+  const index = id.slice(-1);  
+  
   // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
-  // TODO: Save the cart back to local storage
-  // TODO: Re-draw the cart table
-
+  if (xbtn === 'x-button') {
+    cart.removeItem(index);
+    // TODO: Save the cart back to local storage
+    cart.saveToLocalStorage();
+    // TODO: Re-draw the cart table
+    renderCart();
+  }
 }
 
 // This will initialize the page and draw the cart on screen
 renderCart();
-// showCart();
